3af34da30353ae03d3a1be59ca187f6c90747d14
terrordc <nsctfkr@gmail.com>
2023-03-07 18:27:17 +0600

    12312321

--- src/components/+nav.svelte
+++ src/components/+nav.svelte
@@ -1,4 +1,4 @@
-<nav class="flex justify-center w-full">
-    <a class="mx-3 text-lg" href="/">Home</a>
-    <a class="mx-3 text-lg" href="/about">About</a>
+<nav class="flex justify-center items-start w-full bg-gradient-to-b from-slate-500 ... pb-16">
+    <a class="mx-1 text-lg gap-2 bg-slate-500 p-4 text-white font-bold hover:bg-slate-400 transition-all" href="/">Home</a>
+    <a class="mx-1 text-lg gap-2 bg-slate-500 p-4 text-white font-bold hover:bg-slate-400 transition-all" href="/about">About</a>
 </nav>
