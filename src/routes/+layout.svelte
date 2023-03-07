c4dc95d5fc7565a7f4bdd6819ac23682a6855442
terrordc <nsctfkr@gmail.com>
2023-03-07 18:27:33 +0600

    asdasd

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,9 @@
     import Nav from "../components/+nav.svelte";
   </script>
 
-<div class="p-8 max-w-6xl mx-auto">
+<div class="bg-zinc-500">
+  <div class=" px-32 max-w-6xl mx-auto">
     <Nav/>
 <slot></slot>
+</div>
 </div>
